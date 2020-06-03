8a5886aa2fd20c9fb511df62e36f5956493291fe
Sam-aBii <samabii786@gmail.com>
2020-06-04 02:08:31 +0500

    chapter 9 to 11 added

---
+++ JavaScript Projects/Chapter 9 to 11/app.js
@@ -0,0 +1,133 @@
+// // Question 1
+
+// var city = prompt("Enter city name");
+// if(city == "Karachi"){
+//     alert("Wellcome to the City of Lights " + city)
+// }
+
+
+// // Question 2
+
+
+// var gender = prompt("Enter your gender")
+// if(gender == "Male"){
+//     alert("Good Morning Sir")
+// }else if (gender == "female"){
+//     alert("Good Morning Mam")
+// }else{
+//     alert("Good Morning Velli Qoum")
+// }
+
+
+// // Question  3
+
+
+// var color = prompt("Enter color name");
+// if(color == "Red"){
+//     alert("Must Stop")
+// }else if( color == "Yellow"){
+//     alert("Ready to Move")
+// }else if(color == "Green"){
+//     alert("MOve Now")
+// }else{
+//     alert("You are on the way")
+// }
+
+
+// // Question 4
+
+
+// var remainingFuel = prompt("Enter remaining fuel in liters");
+// if(remainingFuel < 0.25){
+//     alert("PLease refill the fuel in your car")
+// }else{
+//     alert("Please carry on")
+// }
+
+
+// // Question 5
+
+
+//     var a = 4;
+//    if (++a === 5) {
+//   alert("given condition for variable a is true");
+//    }
+//    var b = 82;
+//    if (b++ === 83){
+//    alert("given condition for variable b is true");
+//    }
+//    var c = 12;
+//    if (c++ === 13){
+//    alert("condition 1 is true");
+//    }
+//    if (c === 13){
+//    alert("condition 2 is true");
+//    }
+//    if (++c < 14){
+//    alert("condition 3 is true");
+//    }
+//    if(c === 14){
+//    alert("condition 4 is true");
+//    }
+//    var materialCost = 20000;
+//    var laborCost = 2000;
+//    var totalCost = materialCost + laborCost;
+//    if (totalCost === laborCost + materialCost){
+//    alert("The cost equals");
+//    }
+//    if (true){
+//    alert("True");
+//    }
+//    if (false){
+//    alert("False");
+//    }
+
+
+
+    // // Question 6
+
+    // var secreteNum = 5;
+    // var user = prompt("Enter secrte number");
+    // if(user == secreteNum ){
+    //     alert("Bingo! Correct Answer")
+    // }else if (user <= secreteNum+1){
+    //     alert("Close enough to the correct answer")
+    // }else{
+    //     alert("Sorry you are not correct")
+    // }
+
+
+    // // Question 7
+
+    // var temp = prompt("Enter temparture in degree");
+    // if (temp > 40){
+    //     alert("It is too hot outside")
+    // }else if(temp > 30){
+    //     alert("The Weather today is Normal")
+    // }else if( temp > 20){
+    //     alert("Today’s Weather is cool.")
+    // }else if(temp > 10){
+    //     alert("OMG! Today’s weather is so Cool.")
+    // }else{
+    //     alert("Wrong No")
+    // }
+
+
+    // // Question 8 
+
+    // var checkNum = prompt("Enter number");
+    // if(checkNum %2 == 0){
+    // alert("Even Number");
+    // }else{
+    // alert("Odd Number");
+    // };
+
+
+    // // Question 9
+
+    // var number = prompt("Enter number");
+    // if(number %3 == 0 ){
+    //     alert("It is divisible by 3")
+    // }
+
+    
