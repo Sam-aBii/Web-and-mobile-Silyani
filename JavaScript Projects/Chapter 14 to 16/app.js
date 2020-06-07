9c495010eb0bd85a28bf0d1afb2af0cca93719c1
Sam-aBii <samabii786@gmail.com>
2020-06-07 20:08:56 +0500

    class 14 to 16 added

---
+++ JavaScript Projects/Chapter 14 to 16/app.js
@@ -0,0 +1,50 @@
+
+
+//   // Question 1
+
+//   var studentNames = [];
+
+//   // Question 2
+
+
+
+
+
+//   // Question 3
+
+//   var string = ["A","B","C","D","F","G"];
+
+
+//   // Question 4
+
+//   var numbers = [1,2,3,4,5,6,7,8,9,10];
+
+
+//   // Question 5
+
+//   var BooleanArray = [true,false];
+
+
+//   // Question 6
+
+//   var mixedArray = ["Mixed",12,true];
+
+
+//   // Question 7
+
+//   var Qualification = ["SSC","HSC","BSC","BS","BCOM","MS","MPHIL","PHD"]
+
+//   var NewQualification = Qualification.map( name => name)
+
+//   document.write(NewQualification);
+  
+
+  // Question 8
+
+  var Students = ["Abid","Ali","Arif"];
+  var score = [320,230,480];
+
+  var totalScore = 500;
+
+  
+
